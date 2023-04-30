aec1d32a36e2d6f9370a3ad3aac1c2661a8273a9
GadeliaGoderdzi <130245892+GadeliaGoderdzi@users.noreply.github.com>
2023-04-30 03:03:24 -0400

    gg

--- streamlit.py
+++ streamlit.py
@@ -9,7 +9,7 @@ with header:
     st.header('Hello this is my OSINT tool')
 
 try:
-    from search import search
+    from googlesearch.search import search
 except ImportError:
     print("No module named 'google' found")
 list_links = []
