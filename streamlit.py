fa8fcf3d442b48ac8a62813eee0849726461961c
GadeliaGoderdzi <130245892+GadeliaGoderdzi@users.noreply.github.com>
2023-04-30 09:53:20 -0400

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -14,7 +14,7 @@ except ImportError:
     print("No module named 'google' found")
 list_links = []
 def collector(source):
-    for j in search(source):  #tld="co.in",num=20, stop=20, pause=2  
+    for j in search(source,stop=20):  #tld="co.in",num=20, stop=20, pause=2  
         list_links.append(j)
     return list_links
 
